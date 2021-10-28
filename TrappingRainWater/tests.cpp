3a46a8b34428063865aa60efe484716933fd1acb
Domenico <ferraro.domenico125@gmail.com>
2021-10-28 16:24:43 +0200

    Tests for trapping rain water problem

--- TrappingRainWater/tests.cpp
+++ TrappingRainWater/tests.cpp
@@ -5,16 +5,20 @@
 #include <gtest/gtest.h>
 #include "trappingrainwater.h"
 
-// Demonstrate some basic assertions.
-TEST(TrappingRainWaterTest, BasicAssertions) {
-    std::vector<int> vector;
-    vector.reserve(6);
-    vector.push_back(3);
-    vector.push_back(0);
-    vector.push_back(0);
-    vector.push_back(2);
-    vector.push_back(0);
-    vector.push_back(4);
+TEST(TrappingRainWaterTest1, BasicAssertions) {
+    std::vector<int> vector { 3, 0, 0, 2, 0, 4};
     int total_trapped_water = trapping_rain_water(vector);
     EXPECT_EQ(total_trapped_water, 10);
+}
+
+TEST(TrappingRainWaterTest2, BasicAssertions) {
+    std::vector<int> vector { 7, 4, 0, 9};
+    int total_trapped_water = trapping_rain_water(vector);
+    EXPECT_EQ(total_trapped_water, 10);
+}
+
+TEST(TrappingRainWaterTest3, BasicAssertions) {
+    std::vector<int> vector { 6, 9, 9 };
+    int total_trapped_water = trapping_rain_water(vector);
+    EXPECT_EQ(total_trapped_water, 0);
 }

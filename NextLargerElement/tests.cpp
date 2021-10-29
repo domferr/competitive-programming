29eadcd73f370174af521029fdbbb4629b0fc99d
Domenico <ferraro.domenico125@gmail.com>
2021-10-29 09:54:57 +0200

    Some unit tests

--- NextLargerElement/tests.cpp
+++ NextLargerElement/tests.cpp
@@ -5,28 +5,28 @@
 #include <gtest/gtest.h>
 #include "nextlargerelement.h"
 
-TEST(SlidingWindowMaximumTest1, BasicAssertions) {
+TEST(NextLargerElementTest1, BasicAssertions) {
     std::vector<int> elements {1, 3, 2, 4};
     std::vector<int> solution = {3, 4, 4, -1};
     auto res = nextlargerelement(elements);
     EXPECT_EQ(res, solution);
 }
 
-TEST(SlidingWindowMaximumTest2, BasicAssertions) {
+TEST(NextLargerElementTest2, BasicAssertions) {
     std::vector<int> elements {6, 8, 0, 1, 3};
     std::vector<int> solution = {8, -1, 1, 3, -1};
     auto res = nextlargerelement(elements);
     EXPECT_EQ(res, solution);
 }
 
-TEST(SlidingWindowMaximumTest3, BasicAssertions) {
+TEST(NextLargerElementTest3, BasicAssertions) {
     std::vector<int> elements {14, 3, 12, 5, 1, 6, 7};
     std::vector<int> solution = {-1, 12, -1, 6, 6, 7, -1};
     auto res = nextlargerelement(elements);
     EXPECT_EQ(res, solution);
 }
 
-TEST(SlidingWindowMaximumTest4, BasicAssertions) {
+TEST(NextLargerElementTest4, BasicAssertions) {
     std::vector<int> elements {1, 13, 4, 9, 6};
     std::vector<int> solution = {13, -1, 9, -1, -1};
     auto res = nextlargerelement(elements);
